f02bf1a48160d92573a45d9334a3ba75824995f6
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-09-24 10:49:57 +0530

    first commit

---
+++ HelloWorld/Calculation/Cal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld.Calculation
+{
+    public class Cal
+    {
+        public int AddNumber(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using HelloWorld.Calculation;
+using System;
+//using HelloWorld.Calculation;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cal c = new Cal();
+            //c.AddNunber(3, 6);
+            int ca = c.AddNumber(3, 6);
+            Console.WriteLine(ca);
+            Console.WriteLine("Hello Guys!");
+            string x = Console.ReadLine();
+
+            Console.WriteLine(x);
+        }
+    }
+}
